5c70140e90004ec5c53d2c8b44b77db451a74c52
JoFitTech <jos.lautner.24@heilbronn.dhbw.de>
2024-11-12 14:11:50 +0100

    Test

--- src/main/java/com/jofittech/cryptique/Cryptique.java
+++ src/main/java/com/jofittech/cryptique/Cryptique.java
@@ -1,5 +1,14 @@
 package com.jofittech.cryptique;
 
-public class Cryptique {
-    
+import com.cthiebaud.passwordvalidator.PasswordValidator;
+import com.cthiebaud.passwordvalidator.ValidationResult;
+
+public class Cryptique implements PasswordValidator {
+
+    @Override
+    public ValidationResult validate(String passwordtovalidate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+    }
+
 }
