8ce76ec5b3aefafdba6c9c565ecdb753194a339a
LSchirm <luis.schirmbeck@gmail.com>
2024-11-12 14:42:01 +0100

    Ich hab hier nen bisschen was hinzugefügt

--- src/main/java/com/jofittech/Main.java
+++ src/main/java/com/jofittech/Main.java
@@ -1,7 +1,22 @@
 package com.jofittech;
 
+import com.jofittech.cryptique.Cryptique;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //creating new object of the implementation class which validates the password
+        Cryptique validator = new Cryptique();
+
+        System.out.println();
+        // System.out.println("Hello World!");
+        // System.out.println();
+
+        System.out.println();
+        // Aufruf der validate-Methode und Speichern des Rückgabewerts
+        validator.validate("queen");
+        System.out.println();
     }
 }
--- src/main/java/com/jofittech/cryptique/Cryptique.java
+++ src/main/java/com/jofittech/cryptique/Cryptique.java
@@ -5,10 +5,59 @@ import com.cthiebaud.passwordvalidator.ValidationResult;
 
 public class Cryptique implements PasswordValidator {
 
+    ValidationResult validresult = new ValidationResult(true, "The password is invalid!");
+
+    boolean check;
+    boolean textCheck;
+
+    String text = """
+TheBeatles TheRollingStones LedZeppelin Queen PinkFloyd TheWho U2 ACDC TheEagles 
+Nirvana TheBeachBoys Metallica GunsNRoses FleetwoodMac TheDoors REM Radiohead Aerosmith 
+RedHotChiliPeppers TheClash
+                """;
+    String arr[] = text.split(" ");
+
     @Override
     public ValidationResult validate(String passwordtovalidate) {
+
+        System.out.println("Your password-input is: " + passwordtovalidate);
+
+        if (passwordtovalidate.length() > 8) {
+            check = validresult.isValid();
+            System.out.println("Password is long enough! Good Job!");
+
+        } else {
+            check = !validresult.isValid();
+            System.out.println("Password is to short! Please enter a password with min. eight characters!");
+
+        }
+
+        for (String arr1 : arr) {
+            if (passwordtovalidate.toUpperCase().contains(arr1.toUpperCase())) {
+                check = validresult.isValid();
+                System.out.println("Good job! Your password contains min. one of the biggest bands of all time! (according to ChatGPT ;) ) ");
+                System.out.println(arr1);
+                textCheck = true;
+                break;
+            } else {
+                textCheck = false;
+            }
+        }
+
+        if (!textCheck) {
+            check = !validresult.isValid();
+            System.out.println("Please enter a password which contains min. one of the biggest bands of all time! (according to ChatGPT ;) )");
+        }
+
+        if (check) {
+            System.out.println("Password is valid!");
+        } else {
+            System.out.println(validresult.message());
+        }
+
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+        //throw new UnsupportedOperationException("Progamm ends'");
+        return validresult;
     }
 
 }
